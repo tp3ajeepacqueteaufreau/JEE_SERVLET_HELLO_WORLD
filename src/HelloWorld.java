2cca4b75095b9db3231cb943241741830a1e5d13
wfreau <wfreau@gmail.com>
2019-02-12 11:57:59 +0100

    modificaitons

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,6 +2,8 @@
 
 import java.io.IOException;
 import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -14,20 +16,14 @@ import javax.servlet.http.HttpServletResponse;
 public class HelloWorld extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public HelloWorld() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().append("Hello World to: ");
-		response.getWriter().append(request.getParameter("name"));
+	public void service(ServletRequest request, ServletResponse response) throws IOException{
+		String clientName = request.getParameter("name");
+		response.setContentType("text/html"); // Type MIME
+		java.io.PrintWriter out = response.getWriter();
+		out.println("<html><body>");
+		out.println("Hello World " + clientName + " !");
+		out.println("</body></html>");
 	}
+		
 
 }
